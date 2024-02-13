dea5e2d6bd8c994e69b99333eb875c6e9d0a2ac9
dongHun <actgong@gmail.com>
2024-02-13 22:04:22 +0900

    feature - item 관련 개ë° 시작

--- src/main/java/com/example/skptemp/domain/item/entity/Item.java
+++ src/main/java/com/example/skptemp/domain/item/entity/Item.java
@@ -1,6 +1,8 @@
 package com.example.skptemp.domain.item.entity;
 
 import com.example.skptemp.domain.category.entity.Category;
+import com.example.skptemp.global.error.GlobalErrorCode;
+import com.example.skptemp.global.error.GlobalException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,4 +18,16 @@ public class Item {
     private String grade;
     private int count;
     private int wearingLevel;
+
+    public void addItem(Long count){
+        this.count += count;
+    }
+
+    public void removeItem(Long count){
+        this.count -= count;
+    }
+
+    private void validateItemCount(Long count){
+        if(this.count < count) throw new GlobalException(GlobalErrorCode.ITEM_VALID_EXCEPTION);
+    }
 }
