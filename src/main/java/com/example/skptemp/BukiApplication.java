705882918fa5e13781a159c5fc701bde8af02179
dongHun <actgong@gmail.com>
2024-03-08 22:34:47 +0900

    fix - project 이름 변경 및 버전 정보 변경

--- src/main/java/com/example/skptemp/BukiApplication.java
+++ src/main/java/com/example/skptemp/BukiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class SkpTempApplication {
+public class BukiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SkpTempApplication.class, args);
+        SpringApplication.run(BukiApplication.class, args);
     }
 
 }
