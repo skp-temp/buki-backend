5dba76140c05c6a9c19a4e2f4fe879b07d32aee7
dongHun <actgong@gmail.com>
2024-04-12 22:45:19 +0900

    chore: charm api 양ìurl path 수정 및 api 상세 설명 추가

--- src/main/java/com/example/skptemp/domain/charm/controller/CharmController.java
+++ src/main/java/com/example/skptemp/domain/charm/controller/CharmController.java
@@ -13,20 +13,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/charms")
 @RequiredArgsConstructor
 public class CharmController {
 
     private final CharmService charmService;
 
 
-    @PostMapping("/charm")
+    @Operation(summary = "create charm", description = "부적 생성 API")
+    @PostMapping
     public ResponseEntity<ApiResponse<Void>> createCharm(@Valid @RequestBody CreateCharmRequest request) {
         charmService.createCharm(request);
         return ResponseEntity.ok(ApiResponse.ok());
     }
 
-    @PostMapping("/charm/{charmId}/completion")
+    @Operation(summary = "daily goal 완료", description = "일일 목표 달성 처리 API")
+    @PostMapping("/{charmId}/completion")
     public ResponseEntity<ApiResponse<Void>> dailyGoalDone(@Valid @PathVariable Long charmId) {
 
         //TODO userId security에서 가져오는거 수정해야함
@@ -35,23 +37,24 @@ public class CharmController {
         return ResponseEntity.ok(ApiResponse.ok());
     }
 
-    @GetMapping("/charm/{charmId}")
+    @Operation(summary = "getMyCharm", description = "부적 정보 조회 API")
+    @GetMapping("/{charmId}")
     public ResponseEntity<ApiResponse<CharmDetailResponse>> getMyCharm(@PathVariable Long charmId) {
         CharmDetailResponse charm = charmService.getCharm(charmId);
         return ResponseEntity.ok(ApiResponse.ok(charm));
     }
 
 
-    @PutMapping("/charms/{charmId}/setting")
-    @Operation(summary = "부적 설정 변경 API")
+    @PutMapping("/{charmId}/setting")
+    @Operation(summary = "updateCharmSetting", description = "부적 설정 변경 API")
     public ResponseEntity<Object> updateCharmSetting(@PathVariable Long charmId, @Valid @RequestBody CharmSettingUpdateRequest request) {
         charmService.updateCharmSetting(charmId, request);
 
         return ResponseEntity.ok(ApiResponse.ok());
     }
 
-    @PutMapping("/charms/{charmId}")
-    @Operation(summary = "부적 아이템 등등 변경")
+    @PutMapping("/{charmId}")
+    @Operation(summary = "updateCharm", description = "부적 아이템 장착 API")
     @Deprecated
     //TODO 이미지 어떻게 뿌려야 할지 안정해서 나중에 해야함
     public ResponseEntity<Object> updateCharm(@Valid @RequestBody CharmUpdateRequest request) {
