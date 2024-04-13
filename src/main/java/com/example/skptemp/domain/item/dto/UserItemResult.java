11b750a463bac9dfcc5285e279ae39af72a993b9
dongHun <actgong@gmail.com>
2024-04-13 16:48:05 +0900

    fix : 아이템 ì 전정보 조회시 카테고리 정보 ㅜê추가 제공

--- src/main/java/com/example/skptemp/domain/item/dto/UserItemResult.java
+++ src/main/java/com/example/skptemp/domain/item/dto/UserItemResult.java
@@ -7,13 +7,15 @@ import lombok.Getter;
 @Getter
 public class UserItemResult {
     private Long itemId;
+    private Long categoryId;
     private String itemName;
     private ItemType itemType;
     private int count;
 
     @Builder
-    public UserItemResult(Long itemId, String itemName, ItemType itemType, int count){
+    public UserItemResult(Long itemId, Long categoryId, String itemName, ItemType itemType, int count){
         this.itemId = itemId;
+        this.categoryId = categoryId;
         this.itemName = itemName;
         this.itemType = itemType;
         this.count = count;
