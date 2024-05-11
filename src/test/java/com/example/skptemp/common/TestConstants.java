d4d7636b5bf17830f1f52d6b7ffaa26efbca9837
dongHun <actgong@gmail.com>
2024-05-12 01:06:14 +0900

    fix - TEST 용 상수 값들 분리

---
+++ src/test/java/com/example/skptemp/common/TestConstants.java
@@ -0,0 +1,11 @@
+package com.example.skptemp.common;
+
+public class TestConstants {
+    public static final Long TEST_USER_ID = 1L;
+    public static final String TEST_BADGE_NAME_1 = "치어리더";
+    public static final String TEST_BADGE_CONDITION_1 = "응원메시지 10회 보내기";
+    public static final String TEST_BADGE_TIP_DESCRIPTION_1 = "응원메시지 10회 보내기";
+    public static final String TEST_BADGE_NAME_2 = "뽑기왕";
+    public static final String TEST_BADGE_CONDITION_2 = "뽑기 5회 실행";
+    public static final String TEST_BADGE_TIP_DESCRIPTION_2 = "뽑기 5회 실행";
+}
